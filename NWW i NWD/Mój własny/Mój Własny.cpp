3c097c2a087ef2fed0a4fa4db4de16e1c1fc9e99
Bartakt741 <liswlesiexx@gmail.com>
2022-05-25 11:29:00 +0200

    kak

---
+++ NWW i NWD/Mój własny/Mój Własny.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "algorytm wyznaczający największy wspólny dzielnik \n";
+    cout << "wyznacz liczbę a :";
+    cin >> a;
+    cout << "wyznacz liczbę b :";
+    cin >> b;
+    if (a == b) {
+        cout << a;
+    }
+    c = a * b;
+    while (a != b) {
+        
+        if (a == b) {
+            return a;
+        }
+        else {
+            if (a > b) {
+                a = a - b;
+            }
+            else {
+                b = b - a;
+            }
+        }
+    }
+
+    cout << "Największym wspólnym Dzielnikiem jest :" << a << "\n";
+    cout << "NAjmniejsza wspólna wielokrotność to :" << c / a;
+  
+}
