b28c3a37c0ba659cb431071a2540ff3b4b50aab4
Bartakt741 <liswlesiexx@gmail.com>
2022-05-26 11:28:22 +0200

    jko

---
+++ Wyszukiwanie liczb kwadratowych/Kod wyszukujący pierwiastek.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	b = 0;
+	cout << "Program znajdujący równania kwadratowe \n";
+	cout << "podaj liczbę którą chcesz zpierwiastkować :";
+	cin >> c;
+	a = c;	
+	cout << "Podaj pierwiastek :";
+	cin >> b;
+
+	for (int d = 0; d <= b; d++) {
+		a = a * c;
+		cout << a << " \n";
+	}
+	
+}
